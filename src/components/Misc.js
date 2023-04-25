199049b734a62477ad25873a2ab9d1119fb09b67
Lucas Camacho <pessolilucas@gmail.com>
2023-04-25 20:55:27 -0300

    Adicionado cards

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Misc(){
 
@@ -17,9 +18,12 @@ function Misc(){
             var abil = [data.abilities]
             console.log(data)
             setExibir(true);
-            setPokeObj({nome: pokeName, image: pokeImage, abilities: abil});        
+            setPokeObj({nome: pokeName, image: pokeImage, abilities: abil});
+            toast.success("Pokemon encontrado no sistema!")
         })
+        .catch(e => toast.error("Pokemon não encontrado , você escreveu corretamente?"))
     }
+
     return(
         <div>
             <input id="oi" type="text" placeholder="Insira o nome do pokemon" onChange={(e => setBuscaPoke(e.target.value))}/>
@@ -29,23 +33,24 @@ function Misc(){
                     <div>
                         <p className="pokeP">{pokeObj.nome}</p>
                         {console.log(pokeObj.abilities)}
-                        {
-                            pokeObj.abilities[0].map((e, index) =>{
-                                // return <p key={index}>{e.ability.name}</p> return usado dentro de um map no html pra retornar apenas um elemento
-                                return ( // return entre parentes pra envolver mais de um elemento. (bom usar a key na div)
-                                <div key={index}>
-                                    <p>Habilidade {index + 1}: {(e.ability.name).charAt(0).toUpperCase() + (e.ability.name).slice(1)}</p>
+                            <div class="card">
+                                <div class="card-image"><img src={pokeObj.image}/></div>
+                                <div class="category"> <p>{pokeObj.nome}</p> </div>
+                                <div class="heading">
+                                    {
+                                        pokeObj.abilities[0].map((e, index)=>{
+                                            return <p>Habilidade {index + 1}: {(e.ability.name).charAt(0).toUpperCase() + (e.ability.name).slice(1)}</p>
+                                        })
+                                    }
+                                    <div class="author"> By <span class="name">Abi</span> 4 days ago</div>
                                 </div>
-                                )
-
-                            })
-                        }
-                        <img src={pokeObj.image} className="pokeI"/>
-                    </div>
+                                </div>
+                        </div>
                 ) : (
                     null
                 ))
             }
+            <ToastContainer />
         </div>
     )
 }
