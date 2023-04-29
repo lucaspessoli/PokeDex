63371634a55ebc01cb29847d1073040465aacfed
Lucas Camacho <pessolilucas@gmail.com>
2023-04-29 00:35:54 -0300

    Adicionado fileira de vizualizacao dos pokemons, em breve adicionar botao pra navegar aos 10 proximos e arrumar front

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,13 +1,50 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import axios from "axios";
 
 function PokemonCollection(){
 
-    useEffect((e)=>{
-        
-    }, [])
+    const [exibicoes, SetExibicoes] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10`)
+          .then((response) => {
+            const tempExibicoes = [];
+            const requests = response.data.results.map((pk) => {
+                return axios.get(pk.url)
+                    .then((r) => {
+                        tempExibicoes.push(r.data);
+                        console.log(r.data)
+              });
+            });
+            Promise.all(requests)
+              .then(() => {
+                SetExibicoes(tempExibicoes);
+              });
+          });
+      }, []);
+      
 
     return(
-        <h3>POKEDEX</h3>
+        <div>
+        <div>
+            <p>salve</p>
+        </div>
+        <div>
+        {
+        exibicoes.map((poke, index) => {
+            return(
+            <div className="subCard">
+                <div className="header">
+                    <div className="image">
+                        <span className="tag"><img src={poke.sprites.front_default} /></span>
+                    </div>
+                </div>
+            </div>
+            )
+        })
+        }
+        </div>
+        </div>
     )
 }
 
